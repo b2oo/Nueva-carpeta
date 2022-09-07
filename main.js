84c8cd581efd48dd041dbb49c4467a57a4ad5700
b2oo <lautitimrnd@gmail.com>
2022-09-07 19:38:16 -0300

    Proyecto agregado

---
+++ main.js
@@ -0,0 +1,32 @@
+let ciudadano = 11;
+let oveja = 3;
+let casa = 0;
+let jabali = 1;
+    function matarOvejas() {
+        ciudadano = ciudadano - 6;
+        oveja = oveja - 1;
+    }
+    function crearCasa(){
+        ciudadano = ciudadano - 1;
+        casa = casa + 1;
+
+    }
+
+    function matarJabali(){
+        ciudadano = ciudadano - 1;
+        jabali = jabali - 1;
+    }
+    
+    function validacion(value){
+        value = prompt('bro');
+
+            if(value == "xd"){
+                return alert('hola');
+            }else if(value == "XD"){
+                return alert('chau');
+            }else{
+                alert('otra vez')
+                validacion()
+            }
+    }
+
